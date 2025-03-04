f4f1b6541da472717733ff71d3a699fb28683f90
Stephan De Spiegeleire <sdspieg@mail.ru>
2025-03-04 01:26:15 +0100

    Add minimal Flask app for my_document_accordion.html

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='.', static_url_path='')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory('.', 'my_document_accordion.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
